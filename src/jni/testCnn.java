4c20e958f92d7077133b0231a14338cb3e055f6c
zhangqianhui <1632206636@qq.com>
2016-08-11 02:37:14 +0800

    Revert "Revert "image_mean""
    This reverts commit a0718d87741a2c0d529817b932248470bd05e91a.

--- src/jni/testCnn.java
+++ src/jni/testCnn.java
@@ -9,6 +9,7 @@ public class testCnn {
 	public static String tiny_file = Environment.getExternalStorageDirectory() + "/tinyfile/carlogo";
 	public static String caffeModel = Environment.getExternalStorageDirectory() + "/tinyfile/gender.caffemodel";
 	public static String caffePro = Environment.getExternalStorageDirectory() + "/tinyfile/deploy.prototxt";
-
+	public static String caffeMean = Environment.getExternalStorageDirectory() + "/tinyfile/gender_mean.binaryproto";
+	
 	public static native int jniPredict2(long addr , String caffeModel , String caffePro);
 }
